c8d87b889b3ecc1965e314520919f260612a37c4
Zoe Tseng <zoetseng0@gmail.com>
2023-06-26 10:22:18 -0400

    Initial Commit

---
+++ kwkGitHub/kwkGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  kwkGitHubApp.swift
+//  kwkGitHub
+//
+//  Created by Zoe Tseng on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct kwkGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
